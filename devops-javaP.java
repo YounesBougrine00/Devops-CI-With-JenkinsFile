ba263e0d8b83d8e2bbf8ffa47d8e75563ce4b646
Younesbougrine00 <azour1955@gmail.com>
2021-12-16 11:16:05 +0100

    first commit

---
+++ devops-javaP.java
@@ -0,0 +1,79 @@
+package tp2;
+import java.util.Scanner;
+	public static void main(String[] args) {
+	
+
+	System.out.println("Hello World");
+	
+	Object keyb = null;
+	int a = ((Scanner) keyb).nextInt();
+	int b = ((Scanner) keyb).nextInt();
+	int c = ((Scanner) keyb).nextInt();
+	a=c;
+	b=a;
+	c=a;
+	System.out.println("la valeur de a="+a);
+	System.out.println("la valeur de b="+b);
+	System.out.println("la valeur de c"+c);
+}
+	
+	
+	{	System.out.println("Entrez la valeur de a");
+		
+		Scanner keyb=null;
+		int a = ((Scanner) keyb).nextInt();
+		System.out.println("Entrez la valeur de b");
+		int b = ((Scanner) keyb).nextInt();
+	  
+		if (a==b);{
+		  System.out.println(a);
+	  }
+		if (a<b);{
+			a=b;
+			b=a;
+			int r=a%b;
+			while (r!=0); {
+				a=b;
+				b=r;
+				r=a%b;
+			System.out.print("la valeur du pgcd est :"+r);
+			
+			
+			
+	
+				
+			
+				
+			}
+			
+			
+		  
+		  
+	  
+		  
+		  
+		  
+		  
+		  
+	  }
+	}
+	
+}
+
+		
+		
+
+			
+		
+			
+		
+		
+		
+	  
+	  
+  
+	  
+	  
+	  
+	  
+	
