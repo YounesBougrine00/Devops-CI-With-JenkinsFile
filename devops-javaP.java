2cb8946def8c4b039aa14d0275d3fd54467128e5
Abdelhaq-Aj <abdelhaq.ajnaki@gmail.com>
2022-01-01 17:31:16 +0100

    changed by AJNAKI

--- devops-javaP.java
+++ devops-javaP.java
@@ -1,7 +1,9 @@
 package tp2;
 
-public static void main(String[] args) {
-	System.out.println("Hello World !");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
 
 
